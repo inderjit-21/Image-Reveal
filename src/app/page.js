ad33beef9cd25dfca5c38e1106d9ac4b454f5cb6
inderjit-21 <inderjit.contact@gmail.com>
2026-01-05 23:21:03 +0530

    Code Done

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="w-full h-[300vh] flex relative mainCont max-sm:hidden">
       <HeroSection />
     </div>
-    <div className="w-full h-screen flex justify-center text-[18px] leading-[18px] items-center text-[#91531d] px-[7vw]">
+    <div className="w-full h-screen flex justify-center text-[18px] leading-[18px] text-center items-center text-[#91531d] px-[17vw]">
       <p>Open this link on a PC/Laptop for a better experience.</p>
     </div>
     </>
