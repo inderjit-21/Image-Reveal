677410630f2e7c33ba961a2eb259cab056d8010e
inderjit-21 <inderjit.contact@gmail.com>
2026-01-05 23:18:57 +0530

    ALL DONE

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,13 @@ import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="w-full h-[300vh] flex relative mainCont">
+    <>
+    <div className="w-full h-[300vh] flex relative mainCont max-sm:hidden">
       <HeroSection />
     </div>
+    <div className="w-full h-screen flex justify-center text-[18px] leading-[18px] items-center text-[#91531d] px-[7vw]">
+      <p>Open this link on a PC/Laptop for a better experience.</p>
+    </div>
+    </>
   );
 }
